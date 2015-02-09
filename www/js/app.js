775b194be8ac83b2b93f8948e27f1db18a28e382
Patrick R <patrick.rein@gmail.com>
2015-02-09 16:03:22 +0100

    Removes unused dependecies

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,9 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform, $cordovaFile) {
+.run(function($ionicPlatform) {
   ImgCache.options.debug = true;
   ImgCache.options.chromeQuota = 50*1024*1024;
 
