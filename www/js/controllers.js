27029373b493a595dc761d5618da95fed8974169
Patrick R <patrick.rein@gmail.com>
2015-02-20 21:38:30 +0100

    Improves performance of search a bit by using debounce which delays the change of the searchTerm and added fixed categories to events

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -335,11 +335,12 @@ angular.module('starter.controllers', ['starter.services'])
     });
   });
 
+  $scope.categoriesNotToShow = ['Vertiefungsworkshop', 'Unternehmensworkshop'];
   $scope.eventCategoryNames = {};
   $scope.eventCategoryNames[Persistence.Entities.EVENT_TYPES.UNTERNEHMENSWORKSHOP] = 'Unternehmensworkshop';
   $scope.eventCategoryNames[Persistence.Entities.EVENT_TYPES.VERTIEFUNGSWORKSHOP] = 'Vertiefungsworkshop';
-  $scope.eventCategoryNames[Persistence.Entities.EVENT_TYPES.MAIN] = 'Vertiefungsworkshop';
-  $scope.eventCategoryNames[Persistence.Entities.EVENT_TYPES.EVENING] = 'Vertiefungsworkshop';
+  $scope.eventCategoryNames[Persistence.Entities.EVENT_TYPES.MAIN] = 'Hauptveranstaltung';
+  $scope.eventCategoryNames[Persistence.Entities.EVENT_TYPES.EVENING] = 'Rahmenprogramm';
 
   $scope.eventCategoryName = function(event) {
     return $scope.eventCategoryNames[event.eventType];
