4e5129d0962c3c208f1ae48b13e326f3f6e695c4
Patrick R <patrick.rein@gmail.com>
2015-02-23 14:23:17 +0100

    Adds loading of news items from file on startup

--- www/js/services.js
+++ www/js/services.js
@@ -154,8 +154,9 @@ angular.module('starter.services', ['ngResource'])
 
 })
 
-.factory('NewsInterval', function($interval, Persistence) {
+.factory('NewsInterval', function($interval, $http, Persistence) {
   var intervalPromise;
+  var started = false;
 
   var newsIntervalFacade = {
     newsUpdateCounter : 0,
@@ -170,16 +171,37 @@ angular.module('starter.services', ['ngResource'])
     });
   };
 
-  newsIntervalFacade.start = function() {
+  var startUp = function() {
     if(!angular.isDefined(intervalPromise)){
       intervalPromise = $interval(updater, 5/*m*/ * 60/*s*/ * 1000 /*ms*/);
     }
     updater();
   };
 
+  newsIntervalFacade.start = function() {
+    if(!started) {
+    started = true;
+    Persistence.listNews().then(function(newsItems) {
+      if(newsItems.length > 0) {
+        startUp();
+      } else {
+        $http.get('newsItems.json')
+          .success(function(result) {
+            var newNewsItems = angular.fromJson(result);
+            angular.forEach(newNewsItems, function(newsItem) {
+              persistence.add(new Persistence.Entities.News(newsItem));
+            });
+            persistence.flush(startUp);
+          });
+      }
+    });
+    }
+  };
+
   newsIntervalFacade.stop = function() {
     if(angular.isDefined(intervalPromise)) {
       $interval.cancel(intervalPromise);
+      started = false;
     }
   };
 
