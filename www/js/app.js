4804a95bae9ce793cb53bd1404fc8b5acd8a70e6
Patrick R <patrick.rein@gmail.com>
2015-02-23 14:09:43 +0100

    Activates the accessory bar for iOS soft keyboard

--- www/js/app.js
+++ www/js/app.js
@@ -13,11 +13,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngMessages'])
   ImgCache.options.chromeQuota = 30*1024*1024;
 
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
