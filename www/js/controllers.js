3f014e09450b54df41d93cbd5ef3fc8d13dd1b3d
Patrick R <patrick.rein@gmail.com>
2015-02-24 14:59:45 +0100

    Adds special maps views to map controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -326,9 +326,22 @@ angular.module('starter.controllers', ['starter.services'])
 
     $scope.room = {};
 
-    Persistence.getRoom($stateParams.roomId).then(function(room) {
-      $scope.room = room;
-    });
+    $scope.specialRooms = {
+      'og' : {'serverId' : 'og', 'mapImagePath' : 'Lageplan_OG_grau.jpg'},
+      'eg' : {'serverId' : 'eg', 'mapImagePath' : 'Lageplan_EG_grau.jpg'}
+    };
+
+    $scope.isSpecialRoom = function(roomId) {
+      return Object.keys($scope.specialRooms).indexOf(roomId) > -1;
+    };
+
+    if(! $scope.isSpecialRoom($stateParams.roomId)) {
+      Persistence.getRoom($stateParams.roomId).then(function(room) {
+        $scope.room = room;
+      });
+    } else {
+      $scope.room = $scope.specialRooms[$stateParams.roomId];
+    }
 
 })
 
