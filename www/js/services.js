75e6e5e02ed8ad83c51182e535a256715e8ef887
Patrick R <patrick.rein@gmail.com>
2016-02-09 21:48:30 +0100

    Fixes an error with content updates

--- www/js/services.js
+++ www/js/services.js
@@ -118,8 +118,8 @@ angular.module('starter.services', ['ngResource'])
         angular.forEach(Object.keys(apiConfiguration), function(routeKey) {
           $http.get(baseURL.replace("%s", routeKey))
             .success(function(requestResult) {
-              var newTimeStamp = moment(angular.fromJson(requestResult)[0], "MM-DD-YYYY HH:mm");
-              var oldTimeStamp = moment(timestampObjects[routeKey].timestamp, "MM-DD-YYYY HH:mm");
+              var newTimeStamp = moment(angular.fromJson(requestResult)[0], "YYYY-MM-DD HH:mm");
+              var oldTimeStamp = moment(timestampObjects[routeKey].timestamp, "YYYY-MM-DD HH:mm");
               if(newTimeStamp > oldTimeStamp) {
                 Persistence[apiConfiguration[routeKey].refreshFunction]().then(function() {
                   updateInterface[apiConfiguration[routeKey].attributeName] += 1;
